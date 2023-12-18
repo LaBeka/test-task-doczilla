b8df096ca9933fe89853521afa842032ab6f0491
Beka <begayymm@gmail.com>
2023-12-18 15:30:26 +0600

    task #1 is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.taskOne.TextFile;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileName = "file.txt";
+        TextFile.run(fileName);
     }
 }
